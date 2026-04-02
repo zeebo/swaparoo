40690737fb4335b9b5726c0c7da9788a34ee4201
Jeff Wendling <leterip@gmail.com>
2026-04-02 11:18:41 -0400

    add Gen call to Tracker

--- tracker.go
+++ tracker.go
@@ -15,17 +15,26 @@ var (
 	}}
 )
 
-// Tracker allows one to acquire Tokens that come with a monotonically increasing
-// generation number. It does so in a scalable way, and optimizes for the case where
-// not many changes to the generation happen. The zero value is safe to use.
+// Tracker allows one to acquire Tokens that come with a monotonically increasing generation number.
+// It does so in a scalable way, and optimizes for the case where not many changes to the generation
+// happen. The zero value is safe to use.
 type Tracker struct {
 	page atomic.Pointer[counterPage]
 	mu   sync.Mutex // serializes Increment
 }
 
-// Acquire returns a Token that can be used to inspect the current generation.
-// It must be Released before an Increment of the Token's generation can
-// complete. It is safe to be called concurrently.
+// Gen returns the current generation of the Tracker. It is safe to be called concurrently, but may
+// be immediately stale with concurrent calls to Increment.
+func (t *Tracker) Gen() uint64 {
+	page := t.page.Load()
+	if page == nil {
+		return 0
+	}
+	return page.gen
+}
+
+// Acquire returns a Token that can be used to inspect the current generation. It must be Released
+// before an Increment of the Token's generation can complete. It is safe to be called concurrently.
 func (t *Tracker) Acquire() Token {
 	// determine which counter we're going to hold
 	hintI := hintPool.Get()
@@ -50,10 +59,9 @@ func (t *Tracker) Acquire() Token {
 		ctr := &page.ctrs[hint%numCounters].ctr
 		ctr.Acquire()
 
-		// load the page again. if it's still the same page and the pgen is the
-		// same, then we know there was no way the page was reused or
-		// Incremented while we were acquiring the counter, so it's safe to
-		// return a Token with the generation we observed.
+		// load the page again. if it's still the same page and the pgen is the same, then we know
+		// there was no way the page was reused or Incremented while we were acquiring the counter,
+		// so it's safe to return a Token with the generation we observed.
 		pageNext := t.page.Load()
 		if page == pageNext && pgen == pageNext.pgen.Load() {
 			// since we have acquired ctr, we are able to read page.gen
@@ -67,17 +75,15 @@ func (t *Tracker) Acquire() Token {
 	}
 }
 
-// Increment bumps the generation of the Tracker for future Acquire calls and
-// returns a Pending that can be used to Wait until all currently Acquired
-// Tokens with the same generation are Released. It is safe to be called
-// concurrently.
+// Increment bumps the generation of the Tracker for future Acquire calls and returns a Pending that
+// can be used to Wait until all currently Acquired Tokens with the same generation are Released. It
+// is safe to be called concurrently.
 func (t *Tracker) Increment() Pending {
 	// serialize concurrent calls to Increment.
 	t.mu.Lock()
 
-	// read and lazily allocate the current page. we have to do this even with
-	// the mutex because Acquire may be happening which ignores the mutex, so
-	// we have to use CAS to synchronize.
+	// read and lazily allocate the current page. we have to do this even with the mutex because
+	// Acquire may be happening which ignores the mutex, so we have to use CAS to synchronize.
 	page := t.page.Load()
 	if page == nil {
 		page = newCounterPage()
@@ -87,18 +93,17 @@ func (t *Tracker) Increment() Pending {
 		}
 	}
 
-	// store in the next page. no need to CAS because we know we're the only
-	// possible writer to the page variable since Acquire only does a CAS from
-	// nil and the mutex serializes calls to Increment.
+	// store in the next page. no need to CAS because we know we're the only possible writer to the
+	// page variable since Acquire only does a CAS from nil and the mutex serializes calls to
+	// Increment.
 	nextPage := newCounterPage()
 	nextPage.gen = page.gen + 1
 	t.page.Store(nextPage)
 
 	t.mu.Unlock()
 
-	// no one else can be reading/writing to the page header now, so we are safe
-	// to return. synchronization is provided by the atomic loads and stores of
-	// the page pointer itself.
+	// no one else can be reading/writing to the page header now, so we are safe to return.
+	// synchronization is provided by the atomic loads and stores of the page pointer itself.
 	return Pending{
 		page: page,
 		gen:  page.gen,
